94fe7fc5f271420ec5c8b2a48480a572967f0a38
Naiem YF <iritman@gmail.com>
2025-03-08 00:46:47 +0330

    Add AuthProvider wrapper to root layout for session management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AuthProvider from './AuthProvider';
 import "./globals.css";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
-          {children}
-        </Theme>
+        <AuthProvider>
+          <Theme>
+            {children}
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
